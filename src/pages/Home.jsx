668d758cd98c07197b101919d685aef02f451966
Harsh Kumar gupta <hkg8558@gmail.com>
2025-01-16 15:30:01 +0530

    social media added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return(<>
-    <div className="w-screen h-full bg-gray-300">
+    <div className="w-full h-screen bg-emerald-300">
         <h1 className="text-white">Home</h1>
     </div>
     </>)
